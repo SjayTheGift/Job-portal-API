382a3368241e3b07aa8232892a334f2111b4ed4e
Esethu <jonas.esethu24@gmail.com>
2023-06-30 21:42:58 +0200

    add env file

--- products/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Product(models.Model):
-    title = models.CharField(max_length=200)
-    price = models.CharField(max_length=10)
-    
-    def __str__(self):
-        return self.title
--- products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import ProductDetailView, ProductListView
-
-urlpatterns = [
-    path('product/', ProductListView.as_view(), name='product'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_update'),
-]
